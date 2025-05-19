7afa8a89fdabd7b0e32c93577bfc95c10932636d
ssdaniel24 <bo7oaonteg2m@mail.ru>
2025-05-19 14:15:28 +0300

    Revert "[Maps] Cute deleted (#3087)"
    This reverts commit 557bb2b8534db59a67b361d16885abf095756d1a.

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -69,6 +69,7 @@ namespace Content.IntegrationTests.Tests
 			"CorvaxMaus",
 			"CorvaxIshimura",
 			"CorvaxPaper",
+			"CorvaxCute",
             "CorvaxPilgrim",
             "CorvaxSplit",
             "CorvaxTerra",
